8ead636aedf9d7a6c17decbfeefe56c3b1ecf94f
Jose <jrosales0001@gmail.com>
2018-01-24 08:46:24 -0500

    lab 3 initial

---
+++ Lab3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            bool RunProgram = true;
+            while (RunProgram)
+            {
+                Console.WriteLine("Please enter an integer");
+                int Input = int.Parse(Console.ReadLine());
+
+                //Process
+                int Square = Input * Input;
+                int Cube = Input * Input * Input;
+
+                //Output
+                Console.WriteLine("\nNumber     Squared     Cubed");
+                Console.WriteLine("=====      =======     =====");
+
+                for (int i = 1; i <= Input; i++)
+                {
+                    Console.WriteLine("{0}          {1}          {2}", i, Square, Cube);
+                }
+
+                Console.WriteLine("\nRun again (Y/N)?");
+                char DoAgain = char.Parse(Console.ReadLine());
+
+                if (DoAgain == 'y' || DoAgain == 'Y')
+                {
+                    RunProgram = true;                    
+                    Console.Clear();
+                }
+                else
+                {
+                    RunProgram = false;
+                }
+            }
+        }
+    }
+}
